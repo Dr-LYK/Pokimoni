a41a2b21231061e6361cebc685d6f845d43677bc
Vivian Chaizemartin <vivian.chaizemartin@epita.fr>
2018-12-17 17:37:44 +0100

    [LAYOUT][HOME] Home layout file implemented (gradient background, font, color customized)

--- app/src/main/java/com/epita/pokimoni/ui/capture/CaptureFragment.kt
+++ app/src/main/java/com/epita/pokimoni/ui/capture/CaptureFragment.kt
@@ -2,14 +2,13 @@ package com.epita.pokimoni.ui.capture
 
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
-import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import com.epita.pokimoni.R
+import com.google.ar.sceneform.ux.ArFragment
 
-class CaptureFragment : Fragment() {
+class CaptureFragment : ArFragment() {
 
     companion object {
         fun newInstance() = CaptureFragment()
