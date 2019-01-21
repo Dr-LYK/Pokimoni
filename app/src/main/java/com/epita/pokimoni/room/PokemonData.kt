94da4d7e8007b631b0dd9bd615055eefb6d793aa
Vivian Chaizemartin <vivian.chaizemartin@epita.fr>
2019-01-21 11:07:47 +0100

    [DATA][ROOM] First Room data persistence implementation

---
+++ app/src/main/java/com/epita/pokimoni/room/PokemonData.kt
@@ -0,0 +1,17 @@
+package com.epita.pokimoni.room
+
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+
+@Entity(tableName = "pokemonData")
+data class PokemonData(@PrimaryKey(autoGenerate = true)
+                       var id: Long?,
+                       @ColumnInfo(name = "index") var index: Int,
+                       @ColumnInfo(name = "name") var name: String,
+                       @ColumnInfo(name = "type1") var type1: String,
+                       @ColumnInfo(name = "type2") var type2: String,
+                       @ColumnInfo(name = "count") var count: Int) {
+
+    constructor(): this(null, 0, "", "", "", 0)
+}
---
+++ app/src/main/java/com/epita/pokimoni/room/PokemonDataDao.kt
@@ -0,0 +1,24 @@
+package com.epita.pokimoni.room
+
+import android.arch.persistence.room.*
+
+@Dao
+interface PokemonDataDao {
+
+    @Query("SELECT * FROM pokemonData WHERE `index` == :index")
+    fun getByIndex(index: Int): PokemonData
+
+    @Query("SELECT * FROM pokemonData WHERE `name` == :name")
+    fun getByName(name: String): PokemonData
+
+    @Query("SELECT * FROM pokemonData")
+    fun getAll(): List<PokemonData>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(pokemonData: PokemonData)
+
+    @Update(onConflict = OnConflictStrategy.ABORT)
+    fun update(pokemonData: PokemonData)
+
+    @Delete
+}
